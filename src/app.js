344b5a072942b5bcb7acd0395ba9a309d63cb25b
Rafael Cardona <rcardonasanta@gmail.com>
2025-08-04 01:25:56 +0000

    card-generator-finish-rafael

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-import "bootstrap";
-import "./style.css";
 
+window.onload = cardgenerator;
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function cardgenerator() {
+  let simbols = ['♠', '♣', '♥', '♦'];
+  let numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let simbol = simbols[Math.floor(Math.random() * simbols.length)];
+  let number = numbers[Math.floor(Math.random() * numbers.length)];
+
+  document.getElementById('top-simbol').textContent = simbol;
+  document.getElementById('number').textContent = number;
+  document.getElementById('bottom-simbol').textContent = simbol;
+
+  let color;
+  if (simbol === '♥' || simbol === '♦') {
+    color = 'red';
+  } else {
+    color = 'black';
+  }
+
+  document.getElementById('top-simbol').style.color = color;
+  document.getElementById('bottom-simbol').style.color = color;
+  document.getElementById('number').style.color = color;
+}
